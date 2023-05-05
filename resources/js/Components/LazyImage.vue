531c65025736c82da4e46a35b7cf495ae13f00fa
Adam Bailey <adamjbailey7@gmail.com>
2023-05-04 21:45:07 -0700

    improve loading state on image once its visible

--- resources/js/Components/LazyImage.vue
+++ resources/js/Components/LazyImage.vue
@@ -1,25 +1,25 @@
 <template>
     <div
-        v-if="!imageSrc"
-        class="rounded-lg bg-gray-800 h-32 w-full animate-pulse object-cover text-white flex p-2"
+        v-if="isLoading"
+        class="rounded-lg bg-yellow-300 dark:bg-gray-800 h-32 w-full animate-pulse object-cover text-gray-800 dark:text-white flex p-2"
     >
         <span class="m-auto"
             >Sorry {{ username }}! Must be {{ excuse }}...</span
         >
     </div>
-
     <img
+        v-else
         ref="target"
         class="rounded-lg object-cover h-full w-full"
         :src="imageSrc"
-        :alt="imageSrc ? alt : null"
+        :alt="alt"
     />
 </template>
 
 <script setup>
-import { useIntersectionObserver } from "@vueuse/core";
+import { useIntersectionObserver, useImage } from "@vueuse/core";
 import { usePage } from "@inertiajs/inertia-vue3";
-import { ref } from "vue";
+import { ref, computed } from "vue";
 
 const props = defineProps({
     src: {
@@ -28,26 +28,29 @@ const props = defineProps({
     },
     alt: {
         type: String,
-        default: null,
+        default: "image",
     },
 });
 
 // let's make it only lead the image src when it's visible
 const target = ref(null);
-const imageSrc = ref(null);
+const imageSrc = ref("/img/photo-placeholder.png");
 // is this image visible in the viewport?
 const isVisible = ref(false);
+
 useIntersectionObserver(
     target,
     ([{ isIntersecting }], observer) => {
         isVisible.value = isIntersecting;
         if (isIntersecting) {
-            imageSrc.value = props.src;
+            imageSrc.value = props.src || "/img/photo-placeholder.png";
             observer.unobserve(target.value);
         }
     },
     { threshold: 0.2 }
 );
+
+const { isLoading } = useImage({ src: computed(() => imageSrc.value) });
 
 const username = usePage().props.value.auth.user.name;
 const excusesImagesWontLoad = [
