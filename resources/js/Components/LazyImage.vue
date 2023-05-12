ca3b79a109abc486786013f2020f2ba6d35a3bfd
Adam Bailey <adamjbailey7@gmail.com>
2023-05-11 20:29:25 -0700

    clean up UX in the image loading stuff

--- resources/js/Components/LazyImage.vue
+++ resources/js/Components/LazyImage.vue
@@ -1,12 +1,10 @@
 <template>
-    <div
-        v-if="isLoading"
-        class="rounded-lg bg-yellow-300 dark:bg-gray-800 h-32 w-full animate-pulse object-cover text-gray-800 dark:text-white flex p-2"
-    >
-        <span class="m-auto"
-            >Sorry {{ username }}! Must be {{ excuse }}...</span
-        >
-    </div>
+    <img
+        v-if="isLoading || error"
+        class="rounded-lg object-cover h-full w-full"
+        :src="placeholder"
+        alt="placeholder image"
+    />
     <img
         v-else
         ref="target"
@@ -18,7 +16,6 @@
 
 <script setup>
 import { useIntersectionObserver, useImage } from "@vueuse/core";
-import { usePage } from "@inertiajs/inertia-vue3";
 import { ref, computed } from "vue";
 
 const props = defineProps({
@@ -32,43 +29,18 @@ const props = defineProps({
     },
 });
 
-// let's make it only lead the image src when it's visible
+const placeholder = "/img/photo-placeholder.png";
 const target = ref(null);
-const imageSrc = ref("/img/photo-placeholder.png");
-// is this image visible in the viewport?
+const imageSrc = ref(placeholder);
 const isVisible = ref(false);
 
-useIntersectionObserver(
-    target,
-    ([{ isIntersecting }], observer) => {
-        isVisible.value = isIntersecting;
-        if (isIntersecting) {
-            imageSrc.value = props.src || "/img/photo-placeholder.png";
-            observer.unobserve(target.value);
-        }
-    },
-    { threshold: 0.2 }
-);
+useIntersectionObserver(target, ([{ isIntersecting }], observer) => {
+    isVisible.value = isIntersecting;
+    if (isIntersecting) {
+        imageSrc.value = props.src || placeholder;
+        observer.unobserve(target.value);
+    }
+});
 
-const { isLoading } = useImage({ src: computed(() => imageSrc.value) });
-
-const username = usePage().props.value.auth.user.name;
-const excusesImagesWontLoad = [
-    "the WIFI",
-    "the internet",
-    "your dad's fault",
-    "the neighbors dog",
-    "a stinky cockroach",
-    "the fly on a hamburger",
-    "a dirty diaper",
-    "rotten radishes",
-    "a madagascar hissing cockroach",
-    "that screaming baby",
-    "Trump",
-    "a fart",
-];
-const excuse =
-    excusesImagesWontLoad[
-        Math.floor(Math.random() * excusesImagesWontLoad.length)
-    ];
+const { isLoading, error } = useImage({ src: computed(() => imageSrc.value) });
 </script>
