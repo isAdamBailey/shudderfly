31c4d8ce9c067c82f17b63849abaa8c058825828
Adam Bailey <adamjbailey7@gmail.com>
2023-05-11 20:55:20 -0700

    style tweaks

--- resources/js/Components/LazyImage.vue
+++ resources/js/Components/LazyImage.vue
@@ -1,14 +1,14 @@
 <template>
     <img
         v-if="isLoading"
-        :class="`${classes} object-cover h-full w-full`"
+        :class="`${classes} object-cover h-full w-full dark:bg-gray-700`"
         :src="placeholder"
         alt="placeholder image"
     />
     <img
         v-else
         ref="target"
-        :class="`${classes} object-cover h-full w-full`"
+        :class="`${classes} object-cover h-full w-full dark:bg-gray-700`"
         :src="imageSrc"
         :alt="alt"
     />
