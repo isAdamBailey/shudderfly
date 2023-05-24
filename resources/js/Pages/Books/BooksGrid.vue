91d5d6879433a102ca479129debbaf284a8720c2
Adam Bailey <adamjbailey7@gmail.com>
2023-05-24 07:46:38 -0700

    do not show empty uncategorized books container

--- resources/js/Pages/Books/BooksGrid.vue
+++ resources/js/Pages/Books/BooksGrid.vue
@@ -1,45 +1,51 @@
 <template>
-    <h3 class="pl-3 mt-3 text-xl text-gray-100 font-bold dark:text-gray-800">
-        {{ capitalize(category.name) }}
-    </h3>
-    <div v-if="loading" class="h-36 flex justify-center">
+    <div v-if="loading" class="h-36 flex justify-center pt-10">
         <span
             class="animate-pulse text-xl text-gray-100 font-bold dark:text-gray-800"
             >Loading {{ category.name }} books...</span
         >
     </div>
-    <div
-        v-else-if="workingBooks.length > 0"
-        ref="content"
-        class="flex snap-x space-x-5 overflow-x-scroll overflow-y-hidden pb-6 scrollbar scrollbar-thumb-gray-500 scrollbar-thumb-rounded"
-        @scroll="handleScroll"
-    >
-        <Link
-            v-for="book in workingBooks"
-            :key="book.id"
-            :href="route('books.show', book.slug)"
-            class="relative w-48 shrink-0 snap-start rounded-lg bg-white shadow-gray-200/50 transition hover:opacity-80 hover:shadow hover:shadow-gray-300/50"
+    <div v-else-if="workingBooks.length > 0">
+        <h3
+            class="pl-3 mt-3 text-xl text-gray-100 font-bold dark:text-gray-800"
         >
-            <div
-                class="rounded-t-lg absolute inset-x-0 top-0 w-full truncate bg-white/70 py-2.5 text-center text-xl leading-4 text-black backdrop-blur-sm line-clamp-1"
+            {{ capitalize(category.name) }}
+        </h3>
+        <div
+            ref="content"
+            class="flex snap-x space-x-5 overflow-x-scroll overflow-y-hidden pb-6 scrollbar scrollbar-thumb-gray-500 scrollbar-thumb-rounded"
+            @scroll="handleScroll"
+        >
+            <Link
+                v-for="book in workingBooks"
+                :key="book.id"
+                :href="route('books.show', book.slug)"
+                class="relative w-48 shrink-0 snap-start rounded-lg bg-white shadow-gray-200/50 transition hover:opacity-80 hover:shadow hover:shadow-gray-300/50"
             >
-                {{ book.title }}
-            </div>
-            <div
-                v-if="book.excerpt"
-                class="rounded-b-lg absolute inset-x-0 bottom-0 w-full truncate bg-white/70 py-2.5 text-center text-sm leading-4 text-black backdrop-blur-sm line-clamp-1"
-            >
-                {{ book.excerpt }}
-            </div>
-            <div class="h-36">
-                <LazyImage
-                    :src="book.pages[0]?.image_path"
-                    :alt="`${book.title} cover image`"
-                />
-            </div>
-        </Link>
+                <div
+                    class="rounded-t-lg absolute inset-x-0 top-0 w-full truncate bg-white/70 py-2.5 text-center text-xl leading-4 text-black backdrop-blur-sm line-clamp-1"
+                >
+                    {{ book.title }}
+                </div>
+                <div
+                    v-if="book.excerpt"
+                    class="rounded-b-lg absolute inset-x-0 bottom-0 w-full truncate bg-white/70 py-2.5 text-center text-sm leading-4 text-black backdrop-blur-sm line-clamp-1"
+                >
+                    {{ book.excerpt }}
+                </div>
+                <div class="h-36">
+                    <LazyImage
+                        :src="book.pages[0]?.image_path"
+                        :alt="`${book.title} cover image`"
+                    />
+                </div>
+            </Link>
+        </div>
     </div>
-    <div v-else class="h-36 flex justify-center">
+    <div
+        v-else-if="category.name !== 'uncategorized'"
+        class="h-36 flex justify-center"
+    >
         <span class="text-xl text-gray-100 font-bold dark:text-gray-800"
             >There was a problem loading {{ category.name }} books.</span
         >
