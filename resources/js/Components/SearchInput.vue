7d911fdc342eba1c7cefe46fa34d594cb4aec25b
Adam Bailey <adamjbailey7@gmail.com>
2024-02-10 13:05:13 -0800

    add voice search to search input component

--- resources/js/Components/SearchInput.vue
+++ resources/js/Components/SearchInput.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-full px-2 bg-transparent">
+    <div class="w-full px-2 bg-transparent flex">
         <label for="search" class="hidden">Search</label>
         <input
             id="search"
@@ -13,14 +13,27 @@
             type="search"
             @keyup.esc="search = null"
         />
+        <button
+            class="self-center text-amber-200 dark:text-gray-100 ml-2 w-6 h-6"
+            @click="startVoiceRecognition"
+        >
+            <Microphone
+                :class="{
+                    'text-red-500': voiceActive,
+                    'microphone-icon': !voiceActive,
+                }"
+            />
+        </button>
     </div>
 </template>
 
 <script>
 import { defineComponent } from "vue";
 import { usePage } from "@inertiajs/inertia-vue3";
+import Microphone from "@/Components/svg/MicrophoneIcon.vue";
 
 export default defineComponent({
+    components: { Microphone },
     props: {
         routeName: {
             type: String,
@@ -35,6 +48,7 @@ export default defineComponent({
     data() {
         return {
             search: usePage().props.value?.search || null,
+            voiceActive: false,
         };
     },
 
@@ -62,6 +76,40 @@ export default defineComponent({
                 { preserveState: true }
             );
         }, 500),
+
+        startVoiceRecognition() {
+            const recognition = new (window.SpeechRecognition ||
+                window.webkitSpeechRecognition)();
+            recognition.interimResults = true;
+
+            recognition.addEventListener("result", (event) => {
+                const transcript = Array.from(event.results)
+                    .map((result) => result[0])
+                    .map((result) => result.transcript)
+                    .join("");
+
+                if (event.results[0].isFinal) {
+                    this.search = transcript;
+                }
+            });
+
+            // keep the voice active state in sync with the recognition state
+            recognition.addEventListener("start", () => {
+                this.voiceActive = true;
+            });
+
+            recognition.addEventListener("end", () => {
+                this.voiceActive = false;
+            });
+
+            recognition.start();
+        },
     },
 });
 </script>
+
+<style scoped>
+.microphone-icon:active {
+    @apply text-red-500 animate-bounce;
+}
+</style>
---
+++ resources/js/Components/svg/MicrophoneIcon.vue
@@ -0,0 +1,12 @@
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <path
+            fill="currentColor"
+            d="M12 18a6 6 0 0 0 6-6V6A6 6 0 0 0 6 6v6a6 6 0 0 0 6 6zM8 6a4 4 0 0 1 8 0v6a4 4 0 0 1-8 0z"
+        />
+        <path
+            fill="currentColor"
+            d="M21 12a1 1 0 0 0-2 0 7 7 0 0 1-14 0 1 1 0 0 0-2 0 9 9 0 0 0 8 8.94V22h-1a1 1 0 0 0 0 2h4a1 1 0 0 0 0-2h-1v-1.06A9 9 0 0 0 21 12z"
+        />
+    </svg>
+</template>
