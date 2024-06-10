a73a6478abc95e583b30e9fb746f040d8a3eb43f
Adam Bailey <adamjbailey7@gmail.com>
2024-06-09 17:20:35 -0700

    formatting

--- app/Jobs/StoreVideo.php
+++ app/Jobs/StoreVideo.php
@@ -3,28 +3,26 @@
 namespace App\Jobs;
 
 use FFMpeg\Format\Video\X264;
-use Illuminate\Http\File;
-use Illuminate\Support\Facades\Log;
-use ProtoneMedia\LaravelFFMpeg\Support\FFMpeg;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Http\File;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
+use ProtoneMedia\LaravelFFMpeg\Support\FFMpeg;
 
 class StoreVideo implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     protected $video;
+
     protected $path;
 
     /**
      * Create a new job instance.
-     *
-     * @param string $video
-     * @param string $path
      */
     public function __construct(string $video, string $path)
     {
@@ -39,29 +37,27 @@ class StoreVideo implements ShouldQueue
      */
     public function handle()
     {
-        $tempFile = tempnam(sys_get_temp_dir(), 'video') . '.mp4';
+        $tempFile = tempnam(sys_get_temp_dir(), 'video').'.mp4';
 
         try {
             $videoData = Storage::disk('local')->get($this->video);
-
             file_put_contents($tempFile, $videoData);
+
             FFMpeg::fromDisk('local')
                 ->open($this->video)
                 ->export()
-                ->inFormat(new X264)
-                ->resize(640, 480)
+                ->inFormat((new X264)->setKiloBitrate(400)->setAudioKiloBitrate(64))
+                ->resize(512, 288)
                 ->save($tempFile);
 
             $path_parts = pathinfo($this->path);
             $directory = $path_parts['dirname'];
-            $filename = $path_parts['basename'];
+            $filename = pathinfo($path_parts['basename'], PATHINFO_FILENAME).'.mp4';
             $processedFilePath = Storage::disk('s3')->putFileAs($directory, new File($tempFile), $filename);
-
             Storage::disk('s3')->setVisibility($processedFilePath, 'public');
         } catch (\Exception $e) {
-            Log::error('FFmpeg failed: ' . $e->getMessage());
+            Log::error('FFmpeg failed: '.$e->getMessage());
         } finally {
-            unlink($tempFile);
             Storage::disk('local')->delete($tempFile);
             Storage::disk('local')->delete($this->video);
         }
