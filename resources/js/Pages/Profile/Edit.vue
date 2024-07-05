098e3e26e0219621fe6df8f8101d588116aa6dfe
Adam Bailey <adamjbailey7@gmail.com>
2024-07-04 17:05:25 -0700

    personalize the profile for non admins

--- resources/js/Pages/Profile/Edit.vue
+++ resources/js/Pages/Profile/Edit.vue
@@ -26,6 +26,13 @@ defineProps({
         </template>
 
         <div class="py-12">
+            <div
+                v-if="!canEditPages"
+                class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg"
+            >
+                Hi {{ $page.props.auth.user.name }}! Mommy and daddy love you!
+                😘
+            </div>
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
                 <div
                     class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg"
