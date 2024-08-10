ceeb73152472f052dfb194d6fcf44a2eb853f0c9
Adam Bailey <adamjbailey7@gmail.com>
2024-08-10 07:30:27 -0700

    add button only shown to admins to see what voices are available on their device, to troubleshoot some voices not being available

--- resources/js/Pages/Profile/Partials/VoiceSettingsForm.vue
+++ resources/js/Pages/Profile/Partials/VoiceSettingsForm.vue
@@ -1,8 +1,11 @@
 <script setup>
-import { useSpeechSynthesis } from "@/composables/useSpeechSynthesis";
 import { computed } from "vue";
+import { useSpeechSynthesis } from "@/composables/useSpeechSynthesis";
+import { usePermissions } from "@/composables/permissions";
+import Button from "@/Components/Button.vue";
 
 const { voices, selectedVoice, setVoice } = useSpeechSynthesis();
+const { canEditPages } = usePermissions();
 
 const filteredVoices = computed(() =>
     voices.value.filter((voice) => voice.lang === "en-US")
@@ -12,12 +15,27 @@ const firstHalf = computed(() =>
     filteredVoices.value.slice(0, halfLength.value)
 );
 const secondHalf = computed(() => filteredVoices.value.slice(halfLength.value));
+
+function alertVoices() {
+    const voiceDetails = voices.value
+        .map(
+            ({ name, lang, default: isDefault }) =>
+                `Name: ${name}, Language: ${lang}, Default: ${isDefault}`
+        )
+        .join("\n");
+    alert("Here are all available voices in your browser:\n" + voiceDetails);
+}
 </script>
 
 <template>
-    <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-        Voice Settings
-    </h2>
+    <div class="flex justify-between">
+        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+            Voice Settings
+        </h2>
+        <Button v-if="canEditPages" class="ml-3" @click="alertVoices"
+            >Voices</Button
+        >
+    </div>
     <p class="my-3 text-sm text-gray-600 dark:text-gray-400">
         Choose from one of the voices below:
     </p>
