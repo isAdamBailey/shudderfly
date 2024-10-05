8eb675e7a9fcb067eb990e15863d782122d99e5c
Adam Bailey <adamjbailey7@gmail.com>
2024-10-05 08:31:48 -0700

    fix immediate pagination in infinite scroll in uploads

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -44,7 +44,7 @@ class PageController extends Controller
             ->when($filter === 'random', fn ($query) => $query->inRandomOrder())
             ->when($filter === 'youtube', fn ($query) => $query->whereNotNull('video_link')->latest())
             ->when($filter === 'popular', fn ($query) => $query->orderBy('read_count', 'desc'))
-            ->paginate();
+            ->paginate(25);
 
         $photos->appends($request->all());
 
