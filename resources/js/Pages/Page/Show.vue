cd57797063590d0039eb3c0726d58b06857d8d9f
Adam Bailey <adamjbailey7@gmail.com>
2024-10-05 07:34:07 -0700

    fix text alignment on edit page form

--- resources/js/Pages/Page/Show.vue
+++ resources/js/Pages/Page/Show.vue
@@ -1,6 +1,6 @@
 <template>
     <BreezeAuthenticatedLayout>
-        <div class="p-4 overflow-hidden text-center">
+        <div class="p-4 overflow-hidden">
             <div v-if="canEditPages" class="mb-3">
                 <Button
                     v-if="!showPageSettings"
@@ -23,80 +23,85 @@
                     @close-page-form="showPageSettings = false"
                 />
             </div>
-            <LazyLoader
-                v-if="page.media_path"
-                class="max-h-[60vh]"
-                :src="page.media_path"
-                :poster="page.media_poster"
-                :alt="page.description"
-            />
-            <div v-else-if="videoId">
-                <VideoWrapper :id="videoId" :title="page.description" />
-            </div>
-            <div class="flex justify-between">
-                <div
-                    class="px-3 py-3 text-lg text-white"
-                    v-html="page.content"
-                ></div>
-                <div v-if="hasContent" class="ml-3 mt-3 text-right">
-                    <Button
-                        type="button"
-                        :disabled="speaking"
-                        @click="speak(stripHtml(page.content))"
-                    >
-                        <i class="ri-speak-fill text-xl"></i>
-                    </Button>
+            <div class="text-center">
+                <LazyLoader
+                    v-if="page.media_path"
+                    class="max-h-[60vh]"
+                    :src="page.media_path"
+                    :poster="page.media_poster"
+                    :alt="page.description"
+                />
+                <div v-else-if="videoId">
+                    <VideoWrapper :id="videoId" :title="page.description" />
                 </div>
-            </div>
-            <p class="px-3 py-3">
-                <span class="text-xs text-white">
-                    {{ short(page.created_at) }}
-                </span>
-            </p>
-
-            <div class="flex justify-around my-3">
-                <Link
-                    v-if="previousPage"
-                    :href="route('pages.show', previousPage)"
-                    as="button"
-                    class="inline-flex items-center text-white disabled:opacity-25 transition ease-in-out duration-150"
-                    aria-label="previous page"
-                    :disabled="backButtonDisabled"
-                    @click="backButtonDisabled = true"
-                >
-                    <i
-                        class="ri-arrow-left-circle-fill text-6xl rounded-full bg-amber-50 text-amber-800 dark:text-gray-900"
-                    ></i>
-                </Link>
-                <Link
-                    v-if="nextPage"
-                    :href="route('pages.show', nextPage)"
-                    as="button"
-                    class="inline-flex items-center text-white disabled:opacity-25 transition ease-in-out duration-150"
-                    aria-label="next page"
-                    :disabled="nextButtonDisabled"
-                    @click="nextButtonDisabled = true"
-                >
-                    <i
-                        class="ri-arrow-right-circle-fill text-6xl rounded-full bg-amber-50 text-amber-800 dark:text-gray-900"
-                    ></i>
-                </Link>
-            </div>
-            <div class="flex">
-                <Link
-                    :href="
-                        route('books.show', { book: page.book.slug, page: 1 })
-                    "
-                    class="flex items-center"
-                    ><Button>
-                        <i
-                            class="ri-arrow-go-back-fill text-xl text-gray-100"
-                        ></i>
-                        <span class="ml-3 font-bold text-gray-100"
-                            >Go to book</span
+                <div class="flex justify-between">
+                    <div
+                        class="px-3 py-3 text-lg text-white"
+                        v-html="page.content"
+                    ></div>
+                    <div v-if="hasContent" class="ml-3 mt-3 text-right">
+                        <Button
+                            type="button"
+                            :disabled="speaking"
+                            @click="speak(stripHtml(page.content))"
                         >
-                    </Button>
-                </Link>
+                            <i class="ri-speak-fill text-xl"></i>
+                        </Button>
+                    </div>
+                </div>
+                <p class="px-3 py-3">
+                    <span class="text-xs text-white">
+                        {{ short(page.created_at) }}
+                    </span>
+                </p>
+
+                <div class="flex justify-around my-3">
+                    <Link
+                        v-if="previousPage"
+                        :href="route('pages.show', previousPage)"
+                        as="button"
+                        class="inline-flex items-center text-white disabled:opacity-25 transition ease-in-out duration-150"
+                        aria-label="previous page"
+                        :disabled="backButtonDisabled"
+                        @click="backButtonDisabled = true"
+                    >
+                        <i
+                            class="ri-arrow-left-circle-fill text-6xl rounded-full bg-amber-50 text-amber-800 dark:text-gray-900"
+                        ></i>
+                    </Link>
+                    <Link
+                        v-if="nextPage"
+                        :href="route('pages.show', nextPage)"
+                        as="button"
+                        class="inline-flex items-center text-white disabled:opacity-25 transition ease-in-out duration-150"
+                        aria-label="next page"
+                        :disabled="nextButtonDisabled"
+                        @click="nextButtonDisabled = true"
+                    >
+                        <i
+                            class="ri-arrow-right-circle-fill text-6xl rounded-full bg-amber-50 text-amber-800 dark:text-gray-900"
+                        ></i>
+                    </Link>
+                </div>
+                <div class="flex">
+                    <Link
+                        :href="
+                            route('books.show', {
+                                book: page.book.slug,
+                                page: 1,
+                            })
+                        "
+                        class="flex items-center"
+                        ><Button>
+                            <i
+                                class="ri-arrow-go-back-fill text-xl text-gray-100"
+                            ></i>
+                            <span class="ml-3 font-bold text-gray-100"
+                                >Go to book</span
+                            >
+                        </Button>
+                    </Link>
+                </div>
             </div>
         </div>
     </BreezeAuthenticatedLayout>
