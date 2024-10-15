5b7f257ed1e55a0b27794fdec239cd78ce17e95b
Adam Bailey <adamjbailey7@gmail.com>
2024-10-15 11:21:56 -0700

    attempt to fix width on application logo in safari navigation bar

--- resources/js/Layouts/Nav/Navigation.vue
+++ resources/js/Layouts/Nav/Navigation.vue
@@ -22,7 +22,7 @@ const showingNavigationDropdown = ref(false);
             <div class="flex justify-between h-16">
                 <div class="flex">
                     <!-- Logo -->
-                    <div class="flex items-center">
+                    <div class="flex items-center max-w-12">
                         <Link :href="route('welcome')">
                             <ApplicationLogo class="h-14" />
                         </Link>
