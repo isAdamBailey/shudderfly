0fd4eb5629408458226f5d48a7ef0a2b4537ba84
Adam Bailey <adamjbailey7@gmail.com>
2024-11-08 17:41:30 -0800

    add category store controller

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,17 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreCategoryRequest;
 use App\Models\Category;
-use Illuminate\Http\Request;
+use Illuminate\Foundation\Application;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Routing\Redirector;
 
 class CategoryController extends Controller
 {
-    public function index(Request $request)
+    public function index(): JsonResponse
     {
         $categories = Category::all();
 
         return response()->json([
             'categories' => $categories,
         ]);
+    }
+
+    public function store(StoreCategoryRequest $request): Application|Redirector|RedirectResponse
+    {
+        Category::create($request->validated());
+
+        return redirect(route('dashboard'));
     }
 }
--- tests/Feature/CategoriesTest.php
+++ tests/Feature/CategoriesTest.php
@@ -4,12 +4,10 @@ namespace Tests\Feature;
 
 use App\Models\Category;
 use App\Models\User;
-use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Testing\Fluent\AssertableJson;
-use Inertia\Testing\AssertableInertia as Assert;
 use Tests\TestCase;
 
 class CategoriesTest extends TestCase
@@ -30,23 +28,24 @@ class CategoriesTest extends TestCase
         $this->getJson(route('categories.index',))
             ->assertJson(fn (AssertableJson $json) => $json
                 ->has('categories', 20)
-                ->dd()
             );
     }
 
-    public function test_book_is_stored()
+    public function test_category_is_stored()
     {
         $this->actingAs($user = User::factory()->create());
         $user->givePermissionTo('edit pages');
 
+        DB::table('categories')->delete();
+
         $payload = [
-            'name' => $this->faker->word(),
+            'name' => "test category",
         ];
 
         $response = $this->post(route('categories.store', $payload));
 
         $category = Category::first();
-        $this->assertSame($category->tname, $payload['name']);
+        $this->assertSame($category->name, $payload['name']);
 
         $response->assertRedirect(route('dashboard'));
     }
