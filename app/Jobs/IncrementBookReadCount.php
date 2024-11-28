5aa4a2980f584f254656dfd6a1d0ec9c9df197be
Adam Bailey <adamjbailey7@gmail.com>
2024-11-28 07:37:37 -0800

    adjust read count jobs to let newer books catch up with top read

--- app/Jobs/IncrementBookReadCount.php
+++ app/Jobs/IncrementBookReadCount.php
@@ -31,11 +31,20 @@ class IncrementBookReadCount implements ShouldQueue
             return;
         }
 
+        // do not increment if one of the highest count
         $maxReadCount = Book::max('read_count');
         $booksWithMaxReadCount = Book::where('read_count', $maxReadCount)->get();
+        if ($booksWithMaxReadCount->contains($this->book)) {
+            return;
+        }
 
-        if ($booksWithMaxReadCount->count() > 1 || ! $booksWithMaxReadCount->contains($this->book)) {
+        // if in the top count, increment by 0.1, else increment by 1 to let newer books catch up
+        $topBooks = Book::orderBy('read_count', 'desc')->take(30)->pluck('id')->toArray();
+
+        if (! in_array($this->book->id, $topBooks)) {
             $this->book->increment('read_count');
+        } else {
+            $this->book->increment('read_count', 0.1);
         }
     }
 }
--- app/Jobs/IncrementPageReadCount.php
+++ app/Jobs/IncrementPageReadCount.php
@@ -31,11 +31,20 @@ class IncrementPageReadCount implements ShouldQueue
             return;
         }
 
+        // if one of the highest count, do not increment
         $maxReadCount = Page::max('read_count');
         $pagesWithMaxReadCount = Page::where('read_count', $maxReadCount)->get();
+        if ($pagesWithMaxReadCount->contains($this->page)) {
+            return;
+        }
 
-        if ($pagesWithMaxReadCount->count() > 1 || ! $pagesWithMaxReadCount->contains($this->page)) {
+        // if in the top count, increment by 0.1, else increment by 1 to let newer pages catch up
+        $topPages = Page::orderBy('read_count', 'desc')->take(30)->pluck('id')->toArray();
+
+        if (! in_array($this->page->id, $topPages)) {
             $this->page->increment('read_count');
+        } else {
+            $this->page->increment('read_count', 0.1);
         }
     }
 }
