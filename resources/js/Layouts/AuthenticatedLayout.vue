9010cf9320b6f100ca99444f6ba0fb14825f1285
Adam Bailey <adamjbailey7@gmail.com>
2025-08-09 08:31:11 -0700

    Refactor AuthenticatedLayout to improve structure and styling
    This update simplifies the layout by removing unnecessary comments and adjusting padding for the SearchInput component. The overall structure is enhanced for better readability and maintainability, contributing to a cleaner codebase.

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -10,22 +10,18 @@ import Navigation from "@/Layouts/Nav/Navigation.vue";
     <div class="flex flex-col flex-grow bg-gray-900">
       <Navigation />
 
-      <!-- Flash Message -->
       <FlashMessage />
 
-      <!-- Global Search -->
-      <div class="w-full px-2 sm:px-6 lg:px-8 mt-3">
+      <div class="w-full pl-2 sm:pl-6 lg:pl-8 mt-3 pr-8">
         <SearchInput />
       </div>
 
-      <!-- Page Heading -->
       <header v-if="$slots.header" class="border-gray-900">
         <div class="max-w-7xl mx-auto py-2 px-4 sm:px-6 lg:px-8 mt-3">
           <slot name="header" />
         </div>
       </header>
 
-      <!-- Page Content -->
       <main>
         <slot />
       </main>
