6b62ebd52aac5b5bdc0969d521bd304cf0b53a00
Adam Bailey <adamjbailey7@gmail.com>
2025-12-29 15:52:54 -0800

    Refactor useGetYouTubeVideo to use nocookie embed URLs for improved privacy

--- resources/js/composables/useGetYouTubeVideo.js
+++ resources/js/composables/useGetYouTubeVideo.js
@@ -1,54 +1,54 @@
 import { computed, unref } from "vue";
 
 export default function useGetYouTubeVideo(videoLink, settings = {}) {
-  // Convert to computed if it's a function (getter), otherwise wrap in computed for reactivity
-  const videoLinkRef =
-    typeof videoLink === "function"
-      ? computed(videoLink)
-      : videoLink && typeof videoLink === "object" && "value" in videoLink
-      ? videoLink
-      : computed(() => videoLink);
-  const videoId = computed(() => {
-    const currentVideoLink = unref(videoLinkRef);
-    if (currentVideoLink) {
-      try {
-        const urlObj = new URL(currentVideoLink);
-        const params = new URLSearchParams(urlObj.search);
+    // Convert to computed if it's a function (getter), otherwise wrap in computed for reactivity
+    const videoLinkRef =
+        typeof videoLink === "function"
+            ? computed(videoLink)
+            : videoLink && typeof videoLink === "object" && "value" in videoLink
+            ? videoLink
+            : computed(() => videoLink);
+    const videoId = computed(() => {
+        const currentVideoLink = unref(videoLinkRef);
+        if (currentVideoLink) {
+            try {
+                const urlObj = new URL(currentVideoLink);
+                const params = new URLSearchParams(urlObj.search);
 
-        let id;
-        if (currentVideoLink.includes("watch?v=")) {
-          id = params.get("v");
-        } else if (currentVideoLink.includes("playlist?list=")) {
-          id = params.get("list");
-        } else {
-          const parts = currentVideoLink.split("/");
-          id = parts[parts.length - 1].split("?")[0];
+                let id;
+                if (currentVideoLink.includes("watch?v=")) {
+                    id = params.get("v");
+                } else if (currentVideoLink.includes("playlist?list=")) {
+                    id = params.get("list");
+                } else {
+                    const parts = currentVideoLink.split("/");
+                    id = parts[parts.length - 1].split("?")[0];
+                }
+                return id || null;
+            } catch (error) {
+                return null;
+            }
         }
-        return id || null;
-      } catch (error) {
         return null;
-      }
-    }
-    return null;
-  });
+    });
 
-  const isPlaylist = computed(() => {
-    const currentVideoLink = unref(videoLinkRef);
-    return currentVideoLink && currentVideoLink.includes("playlist?list=");
-  });
-  const controls = settings.noControls ? "&controls=0" : "";
+    const isPlaylist = computed(() => {
+        const currentVideoLink = unref(videoLinkRef);
+        return currentVideoLink && currentVideoLink.includes("playlist?list=");
+    });
+    const controls = settings.noControls ? "&controls=0" : "";
 
-  const embedUrl = computed(() => {
-    if (!videoId.value) {
-      return null;
-    }
+    const embedUrl = computed(() => {
+        if (!videoId.value) {
+            return null;
+        }
 
-    if (isPlaylist.value) {
-      return `https://www.youtube.com/embed/videoseries?list=${videoId.value}&modestbranding=1&rel=0&playsinline=1${controls}`;
-    } else {
-      return `https://www.youtube.com/embed/${videoId.value}?modestbranding=1&rel=0&playsinline=1${controls}`;
-    }
-  });
+        if (isPlaylist.value) {
+            return `https://www.youtube-nocookie.com/embed/videoseries?list=${videoId.value}&modestbranding=1&rel=0&playsinline=1${controls}`;
+        } else {
+            return `https://www.youtube-nocookie.com/embed/${videoId.value}?modestbranding=1&rel=0&playsinline=1${controls}`;
+        }
+    });
 
-  return { videoId, embedUrl, isPlaylist };
+    return { videoId, embedUrl, isPlaylist };
 }
